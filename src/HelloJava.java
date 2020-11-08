bce1451c8430ae120372ff330cc8f40b5d9de9f7
CYZ <1822550890@qq.com>
2020-11-08 09:52:48 +0000

    calculator

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,16 +1,14 @@
 
 public class HelloJava {
 	
-	private static String say = "happy";
+	private static int a = 5;
+	private static int b = 2;
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		System.out.println("hello git");
-		System.out.println("创建分支");
-		System.out.println("主线添加");
-		System.out.println("分支添加");
-		System.out.println("第二次更新");
-		System.out.println("在线编辑");
-		System.out.println(say);
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
 	}
 
 }
