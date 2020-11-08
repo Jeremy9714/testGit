d24eb8d50cd4cacd8cd3be544c18f5b7873782e5
CYZ <1822550890@qq.com>
2020-11-08 09:39:05 +0000

    task1 change

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,6 +1,7 @@
 
 public class HelloJava {
-
+	
+	private static String say = "happy";
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		System.out.println("hello git");
@@ -9,6 +10,7 @@ public class HelloJava {
 		System.out.println("分支添加");
 		System.out.println("第二次更新");
 		System.out.println("在线编辑");
+		System.out.println(say);
 	}
 
 }
