660074e57fbb4043120f22a08b75a8342a70bcb0
CYZ <1822550890@qq.com>
2020-11-10 11:16:36 +0000

    string

--- src/Join.java
+++ src/Join.java
@@ -3,7 +3,9 @@ public class Join {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-
+		int booktime = 4;
+		float practice = 2.5f;
+		System.out.println("reading time: " + booktime + "\tpractice time:" + practice);
 	}
 
 }
