7f0e06e8cc2d2643622bbf883a70ca46f21f86ee
CYZ <1822550890@qq.com>
2020-11-10 11:22:15 +0000

    break outloop

--- src/outBreak.java
+++ src/outBreak.java
@@ -3,7 +3,12 @@ public class outBreak {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-
+		Loop:for(int i = 0; i<6; ++i) {
+			for (int j = 0; j<6; ++j) {
+				if (j==4) break Loop;
+				System.out.println("i: " + i + " j: " + j);
+			}
+		}
 	}
 
 }
