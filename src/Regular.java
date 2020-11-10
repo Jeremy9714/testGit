d71cd65f4b58bff34d5105dd72b0886f0ae4415e
CYZ <1822550890@qq.com>
2020-11-10 15:21:22 +0000

    regex

--- src/Regular.java
+++ src/Regular.java
@@ -3,7 +3,12 @@ public class Regular {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
+		String A = new String("192.168.0.1");
+		String[] firstArray = A.split("\\.");
+		for (String a:firstArray) System.out.println(a);
 		
+		String[] secondArray = A.split("\\.", 2);
+		for (String a:secondArray) System.out.println(a);
 	}
 
 }
