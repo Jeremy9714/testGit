54e29d6eeab910bbc8d32963b6e846883f128669
CYZ <1822550890@qq.com>
2020-11-12 16:09:14 +0000

    直接选择排序

--- src/ArraySort.java
+++ src/ArraySort.java
@@ -6,9 +6,14 @@ public class ArraySort {
 		// TODO 自动生成的方法存根
 		int arr[] = {63,4,24,1,3,15};
 		System.out.println(Arrays.toString(arr));
+		BubbleSort bSorter = new BubbleSort();
+		bSorter.sort(arr);
 		System.out.println();
-		BubbleSort sorter = new BubbleSort();
-		sorter.sort(arr);
+		
+		int arr2[] = {63,4,24,1,3,15};
+		System.out.println(Arrays.toString(arr2));
+		SelectSort sSorter = new SelectSort();
+		sSorter.sort(arr2);
 	}
 
 }
@@ -26,7 +31,25 @@ class BubbleSort{
 			showArray(arr);
 		}
 	}
-	public void showArray(int[] arr) {
+	public static void showArray(int[] arr) {
 		System.out.println(Arrays.toString(arr));
+	}
+}
+
+class SelectSort{
+	public void sort(int[] arr) {
+		int index;
+		for(int i=1; i<arr.length;++i) {
+			index=0;
+			for(int j=1;j<arr.length-i;++j) {
+				if(arr[j]>arr[index])
+					index = j;
+			}
+			int temp = arr[arr.length-i];
+			arr[arr.length-i] = arr[index];
+			arr[index] = temp;
+			
+			BubbleSort.showArray(arr);
+		}
 	}
 }
