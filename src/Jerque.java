022c621fabe6a6dc941802629b0d7c08a1a0442e
CYZ <1822550890@qq.com>
2020-11-12 12:25:08 +0000

    StringBuilder

--- src/Jerque.java
+++ src/Jerque.java
@@ -2,8 +2,24 @@
 public class Jerque {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
+		String str = "";
+		long starTime = System.currentTimeMillis();
+		for(int i=0;i<10000;++i) {
+			str+=i;
+		}
+		long endTime = System.currentTimeMillis();
+		long time = endTime - starTime;
+		System.out.println("消耗时间"+time);
 		
+		StringBuilder builder = new StringBuilder("");			//创建字符串生成器
+		starTime = System.currentTimeMillis();
+		for(int i=0;i<10000;++i) {
+			builder.append(i);
+		}
+		endTime = System.currentTimeMillis();
+		time = endTime - starTime;
+		System.out.println("消耗时间"+time);
+		//System.out.println(builder.toString());
 	}
 
 }
