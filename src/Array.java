6649612dd2549ce70b262fbf25e9fa03cd02e7fe
CYZ <1822550890@qq.com>
2020-11-12 14:22:40 +0000

    二维数组

--- src/Array.java
+++ src/Array.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Array {
 
@@ -6,6 +7,17 @@ public class Array {
 		int day[] = new int[] {31,29,31,30,31,30,31,31,30,31,30,31};
 		for(int i=0;i<day.length;++i)
 			System.out.println((i+1)+"月有"+day[i]+"天");
+		
+		final int ROW =3, COL =4;
+		int num = 0;
+		int a[][] = new int[ROW][COL];
+		for(int i=0;i<ROW;++i) {
+			for(int j=0;j<COL;++j) {
+				a[i][j] = num;
+				num++;
+			}
+		}
+		System.out.println(Arrays.deepToString(a));
 	}
 
 }
