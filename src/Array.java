04a4104d4a0c00e684d18450e6f96e4bccc451fa
CYZ <1822550890@qq.com>
2020-11-12 14:09:06 +0000

    v1

---
+++ src/Array.java
@@ -0,0 +1,11 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		int day[] = new int[] {31,29,31,30,31,30,31,31,30,31,30,31};
+		for(int i=0;i<day.length;++i)
+			System.out.println((i+1)+"月有"+day[i]+"天");
+	}
+
+}
