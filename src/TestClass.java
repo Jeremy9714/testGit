ae12412bc2a750b9e2aa52b9a9433b95ffe8bf21
CYZ <1822550890@qq.com>
2020-11-13 11:39:11 +0000

    构造函数

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,7 +3,12 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-
+		new CreateObject();
 	}
 
+}
+class CreateObject{
+	public CreateObject() {
+		System.out.println("无参构造函数");
+	}
 }
