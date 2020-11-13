26f2b2be3188c67a9a79aba2a801a0b76f81d2c0
CYZ <1822550890@qq.com>
2020-11-13 12:35:42 +0000

    Integer类

--- src/PackageClass.java
+++ src/PackageClass.java
@@ -3,7 +3,32 @@ public class PackageClass {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-
+		String str[] = {"89","12","10","18","35"};
+		int sum = 0;
+		for(String i:str) {
+			int temp = Integer.parseInt(i);
+			sum+=temp;
+		}
+		System.out.println(sum);
+		Integer in = new Integer("34");
+		System.out.println(in.intValue());
+		System.out.println(in.toString());
+		Integer in2 = Integer.valueOf("55");
+		System.out.println(in2.intValue());
+		
+		String str1 = Integer.toString(456);
+		String str2 = Integer.toBinaryString(456);
+		String str3 = Integer.toHexString(456);
+		String str4 = Integer.toOctalString(456);
+		System.out.println("十进制"+str1);
+		System.out.println("二进制"+str2);
+		System.out.println("十六进制"+str3);
+		System.out.println("八十进制"+str4);
+		
+		int maxint = Integer.MAX_VALUE; //int类型的最大值
+		int minint = Integer.MIN_VALUE; //int类型的最小值
+		int sizeint = Integer.SIZE; //int值的位数
+		System.out.println(maxint +"\t"+minint+"\t"+sizeint);
 	}
 
 }
