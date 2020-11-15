cc3357526c672d0bf63f7684b7f7bd0ed6db93b2
CYZ <1822550890@qq.com>
2020-11-15 12:50:39 +0000

    version final

--- src/ArrayStackDemo.java
+++ src/ArrayStackDemo.java
@@ -1,36 +1,47 @@
+import java.util.Scanner;
 
 public class ArrayStackDemo {
 
 	public static void main(String[] args) {
 		//数组模拟栈
 		ArrayStack s = new ArrayStack(10);
-		try {
-			s.push(1);
-			s.push(2);
-			s.push(3);
-			s.push(4);
-			s.push(5);
-			s.showStack();
-			System.out.println();
-			
-			s.pop();
-			s.pop();
-			s.push(6);
-			s.push(7);
-			s.showStack();
-			System.out.println();
-			/*s.pop();
-			s.pop();
-			s.pop();
-			s.pop();
-			s.pop();
-			s.pop();
-			*/
-		}catch(Exception e) {
-			System.out.println("error");
-		}finally {
-			System.out.println("执行结束");
+		String key = "";
+		Scanner scanner = new Scanner(System.in);
+		boolean loop = true;
+		while(loop) {
+			System.out.println("show表示显示栈");
+			System.out.println("push表示添加数据到栈");
+			System.out.println("pop表示从栈里取出数据");
+			System.out.println("exit表示退出程序");
+			System.out.println("请输入选择");
+			key = scanner.next();
+			switch(key) {
+			case "show":
+				s.showStack();
+				break;
+			case "push":
+				System.out.println("请输入要添加的值");
+				int value = scanner.nextInt();
+				s.push(value);
+				break;
+			case "pop":
+				try {
+					int res = s.pop();
+					System.out.printf("pop出的元素为%d\n",res);
+				}catch(Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			case "exit":
+				scanner.close();
+				loop=false;
+				break;
+			default:
+				break;
+			}
 		}
+		System.out.println("程序结束");
+		
 	}
 
 }
