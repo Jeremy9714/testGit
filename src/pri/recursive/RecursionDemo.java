9e86933e751fbc4ca6cce96e605f2f84baa4cb3f
CYZ <1822550890@qq.com>
2020-11-17 16:14:23 +0000

    final

--- src/pri/recursive/RecursionDemo.java
+++ src/pri/recursive/RecursionDemo.java
@@ -1,10 +1,27 @@
 package pri.recursive;
 
 public class RecursionDemo {
-
+	//static int res=0;
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
 
+		test1(20);
+		//System.out.println(res);
+		System.out.println(factorial(5));
 	}
-
+	//递归
+	public static void test1(int n) {
+		if(n>2) {
+			test1(n-1);
+		}else {
+			//res+=n;
+			System.out.println(n);
+		}
+	}
+	//阶乘
+	public static int factorial(int n) {
+		if(n==1)
+			return 1;
+		else
+			return factorial(n-1)*n;
+	}
 }
