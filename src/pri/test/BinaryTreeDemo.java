558198fb2d75a34ef56680551832c6c47a31102d
CYZ <1822550890@qq.com>
2020-11-22 14:10:26 +0000

    debug

--- src/pri/test/BinaryTreeDemo.java
+++ src/pri/test/BinaryTreeDemo.java
@@ -3,8 +3,35 @@ package pri.test;
 public class BinaryTreeDemo {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
+		
+		int[] arr = {1,2,3,4,5,6,7};
+		ArrayBinaryTree binaryTree = new ArrayBinaryTree(arr);
+		binaryTree.preOrder();
 
 	}
 
+}
+
+class ArrayBinaryTree{
+	private int[] arr;
+	public ArrayBinaryTree(int[] arr) {
+		this.arr = arr;
+	}
+	
+	public void preOrder() {
+		this.preOrder(0);
+	}
+	
+	public void preOrder(int index) {
+		if(arr==null||arr.length==0) {
+			System.out.println("数组为空，遍历失败");
+		}
+		System.out.println(arr[index]);
+		if(index*2+1<arr.length) {
+			preOrder(index*2+1);
+		}
+		if(index*2+2<arr.length) {
+			preOrder(index*2+2);
+		}
+	}
 }
