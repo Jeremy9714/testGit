e5550f153e9ee9e1e8cfd5abc3306d309d2388ed
CYZ <1822550890@qq.com>
2020-11-26 20:20:42 +0000

    二分算法

--- src/pri/algorithms/BinarySearchDemo.java
+++ src/pri/algorithms/BinarySearchDemo.java
@@ -3,8 +3,24 @@ package pri.algorithms;
 public class BinarySearchDemo {
 
 	public static void main(String[] args) {
-		
-
+		int[] arr = {1,2,3,4,5,6,7,8,9};
+		System.out.println(BinarySearch(arr,9));
 	}
-
+	
+	public static int BinarySearch(int[] arr,int value) {
+		int left = 0;
+		int right = arr.length-1;
+		//int mid = (left+right)/2;
+		while(left<=right) {
+			int mid = (left+right)/2;
+			if(arr[mid]==value) {
+				return mid;
+			}else if(value<arr[mid]) {
+				right=mid-1;
+			}else {
+				left=mid+1;
+			}
+		}
+		return -1;
+	}
 }
