3631ac93139873a5c854188ea063fa4f40d1bee9
CYZ <1822550890@qq.com>
2020-12-23 17:05:30 +0000

    分治算法完成

--- src/review/algorithms/DACAlgorithm.java
+++ src/review/algorithms/DACAlgorithm.java
@@ -4,7 +4,17 @@ public class DACAlgorithm {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-
+		tower(3,'a','b','c');
 	}
-
+	
+	public static void tower(int num, char a, char b, char c) {
+		if(num==1) {
+			System.out.println(a+"->"+c);
+		}else {
+			tower(num-1,a,c,b);
+			System.out.println(a+"->"+c);
+			tower(num-1,b,a,c);
+		}
+	}
+	
 }
